8b3d90b02862b0c44dee9c211022113849220667
leoleon1991 <leo.leon1991@gmail.com>
2018-05-15 21:20:26 -0400

    Creacion de los templates y urls para recuperar contraseña por correo

--- ambulatorio/settings.py
+++ ambulatorio/settings.py
@@ -133,3 +133,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  
 
 LOGIN_REDIRECT_URL = reverse_lazy('paciente:index_paciente')
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'leo.leon1991@gmail.com'
+EMAIL_HOST_PASSWORD = '-leoleon08071991luna-'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
